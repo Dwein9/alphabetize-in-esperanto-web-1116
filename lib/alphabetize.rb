0e71fabdcdca3b88c174629d8a327dfe2996ea34
David Weinstein <dwein9@gmail.com>
2016-11-25 21:50:53 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+
+
+  arr.sort_by! do |word|
+    abc_word = []
+    word.each_char do |letter|
+      abc_word << ALPHABET.index(letter)
+    end
+    abc_word
+  end
+  arr
 end
